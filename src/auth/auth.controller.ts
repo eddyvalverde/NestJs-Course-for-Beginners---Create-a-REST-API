96b08eb86deb4dbc59f428152f22954afbccfebb
Eddy Valverde <eddygarros@hotmail.com>
2023-01-14 17:35:09 -0600

    chore: implement request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signin')
-  login() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  login(@Req() req: Request) {
+    console.log(req);
+    console.log(req.body);
     return this.authService.login();
   }
   @Post('signup')
