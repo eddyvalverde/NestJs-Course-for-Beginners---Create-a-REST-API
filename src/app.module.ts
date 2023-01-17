16f7610c9e55bf66e821c10bc7f127b0bf913d5f
Eddy Valverde <eddygarros@hotmail.com>
2023-01-16 21:04:22 -0600

    chore: use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, PrismaModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
