491c129ef1c99d40b3311bbeb07a2cf04d3abaa6
Eddy Valverde <eddygarros@hotmail.com>
2023-01-18 15:01:08 -0600

    chore: simulate appmodule

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 describe('App e2e', () => {
-  /*beforeAll(async () => {
+  beforeAll(async () => {
     const moduleRef =
       await Test.createTestingModule({
         imports: [AppModule],
       }).compile();
     const app = moduleRef.createNestApplication();
-  });*/
+  });
   it.todo('should pass');
 });
